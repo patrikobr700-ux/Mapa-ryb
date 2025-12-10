e8d57f7cfe32a2651b6a04c9aeefdc12f30b7658
patrikobr700-ux <patrikobr700@gmail.com>
2025-12-10 21:42:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,7 @@
-const CACHE_NAME = 'fishingmap-v1';
-const FILES_TO_CACHE = [
-  '/index.html',
-  '/manifest.webmanifest',
-  '/sw.js',
-  '/data/datapoints.json'
-];
-
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', evt => {
-  evt.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(resp => resp || fetch(evt.request))
-  );
+self.addEventListener("activate", () => {
+  console.log("Service Worker aktivní.");
 });
